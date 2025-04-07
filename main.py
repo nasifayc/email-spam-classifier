4e3b8cc40ccfe4061d20bd567dfdcccb8bf1f484
nasifayc <nasifayc11@gmail.com>
2025-04-08 00:50:30 +0300

    feat: 0 for spam 1 for ham

--- main.py
+++ main.py
@@ -10,5 +10,15 @@ df = pd.read_csv('mail_data.csv')
 # print(df)
 
 data = df.where(pd.notnull(df), '')
-r = data.head(10)
-print(r)
+# r = data.head(10)
+# print(r) return first 10 data sets
+
+print(data.info()) #meta data about the data
+
+
+data.loc[data['Category'] == 'spam', 'Category'] = 0
+data.loc[data['Category'] == 'ham', 'Category'] = 1
+
+X, Y = data['Message'], data['Category']
+
+# split the data into test and train dataset
